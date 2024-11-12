1782a029fc76e0dd9f2e158432f38b608192ec66
antonio <antoniogc59@educastur.es>
2024-11-12 16:36:47 +0100

    Initial commit

---
+++ Datathon/js/scripts.js
@@ -0,0 +1,48 @@
+function calculateScore() {
+    const form = document.getElementById('quizForm');
+    const questions = form.querySelectorAll('.question-box');
+    let score = 0;
+
+    questions.forEach((question, index) => {
+        const selectedOption = question.querySelector('input[name="q' + (index + 1) + '"]:checked');
+        const explanation = document.getElementById('q' + (index + 1) + '-explanation');
+
+        if (selectedOption) {
+            if (selectedOption.value === "1") {
+                score++;
+                explanation.classList.add('correct');
+                explanation.classList.remove('incorrect', 'unanswered');
+            } else {
+                explanation.classList.add('incorrect');
+                explanation.classList.remove('correct', 'unanswered');
+            }
+            explanation.style.display = 'block';
+        } else {
+        
+            explanation.style.display = 'block';
+            explanation.classList.add('unanswered');
+            explanation.classList.remove('correct', 'incorrect');
+        }
+    });
+
+    const totalQuestions = questions.length;
+    const percentageScore = ((score / totalQuestions) * 100).toFixed(2);
+    
+    const resultElement = document.getElementById('result');
+    resultElement.textContent = "Score: "+percentageScore+" %";
+
+    if (percentageScore > 50) {
+        resultElement.style.color = 'green';
+    } else {
+        resultElement.style.color = 'red';
+    }
+
+    resultElement.scrollIntoView({ behavior: 'smooth' });
+
+    document.getElementById('sendButton').disabled = true;
+    document.getElementById('exitButton').style.display = 'inline';
+}
+
+function exitQuiz() {
+    window.location.href = '/html/main.html'; 
+}
